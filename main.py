e628a056e77765feebf8e21d5fdb3bfff0c51fa4
Ahmed Abdelhak <ahmed.abdelhak92@gmail.com>
2022-04-12 17:54:00 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+import json
+import os
+
+from scrapy.crawler import CrawlerProcess
+from scrapy.http import Response
+from scrapy.spiders import Spider
+
+
+class BookingSpider(Spider):
+    name = 'booking'
+    start_urls = [f'file://{os.path.abspath("task-booking.html")}']
+    results = {}
+
+    def parse(self, response: Response):
+        self.results["title"] = response.css('#hp_hotel_name::text').get().strip()
+        self.results["address"] = response.css('[data-node_tt_id="location_score_tooltip"]::text').get().strip()
+        self.results["stars"] = response.css('.hp__hotel_ratings i::attr(class)').re_first('.*([0-5]+)')
+
+        self.results["review_points"] = response.css('.average.js--hp-scorecard-scoreval::text').get()
+        self.results["number_of_reviews"] = response.css('.trackit.score_from_number_of_reviews .count::text').get()
+
+        self.results["description"] = '\n'.join(
+            response.css('div.hotel_description_wrapper_exp p').xpath('string()').extract())
+        self.results["room_categories"] = response.css('#maxotel_rooms .ftd').xpath('normalize-space(text())').extract()
+
+        alternative_hotels = []
+        for hotel in response.css('#althotelsRow td'):
+            item = dict()
+            item['link'] = hotel.css('td .althotel_link::attr(href)').get('')
+            item['name'] = hotel.css('td .althotel_link::text').get('')
+            item['rating'] = hotel.css('td .average.js--hp-scorecard-scoreval::text').get('')
+            alternative_hotels.append(item)
+        self.results["alternative_hotels"] = alternative_hotels
+
+    def close(self, spider, reason):
+        with open('output_data.json', 'w') as output_file:
+            json.dump(self.results, output_file)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    process = CrawlerProcess()
+    process.crawl(BookingSpider)
+    process.start()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ readme.md
@@ -0,0 +1,11 @@
+### Running the project
+
+* Go to project directory (fynd-challenge) & Create virtual env 
+  * `python3 -m venv .`
+* activate your virtual environment:
+  * `source venv/bin/activate`
+* install scrapy & Twisted with these versions as they are the ones used when developed and compatible with each other.
+  * `pip3 install scrapy==1.7.4`
+  * `pip3 install Twisted==20.3.0`
+* Now run the main file which will extract the data required from `task-booking.html` and export it to `output_data.json` as json formatted stream
+  * `python3 main.py`
